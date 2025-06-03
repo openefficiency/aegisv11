ede343b2817f621e60382d17c21a2e1d19eb8448
Ali Can Acar <alicanacar@Alis-MacBook-Pro.local>
2025-06-03 10:58:32 -0700

    Move map folder to ethics-officer directory

---
+++ app/dashboard/ethics-officer/map/page.tsx
@@ -0,0 +1,13 @@
+"use client";
+
+import { DashboardLayout } from "@/components/dashboard-layout";
+
+export default function MapDashboard() {
+  return (
+    <DashboardLayout role="investigator">
+      <div className="flex items-center justify-center h-[calc(100vh-4rem)]">
+        <div className="text-white text-2xl">test</div>
+      </div>
+    </DashboardLayout>
+  );
+}
