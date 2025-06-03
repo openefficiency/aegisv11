ac376a076444098408000338a0fd4e923de036c9
Ali Can Acar <alicanacar@Alis-MacBook-Pro.local>
2025-06-03 11:04:46 -0700

    Move map folder to correct location under ethics-officer

---
+++ app/dashboard/ethics-officer/map/loading.tsx
@@ -0,0 +1,11 @@
+import { DashboardLayout } from "@/components/dashboard-layout";
+
+export default function MapLoading() {
+  return (
+    <DashboardLayout role="investigator">
+      <div className="flex items-center justify-center h-[calc(100vh-4rem)]">
+        <div className="text-white">Loading map...</div>
+      </div>
+    </DashboardLayout>
+  );
+} 
